3aaea3aba90360b7b72e732db41b5db114dc6535
reikopi <rk02.stmi.1018@gmail.com>
2022-11-18 23:18:27 +0000

    [Add]java_study_条件分岐

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ let hello = 'Hello World';
 alert(hello);
 
 alert('Hello'+'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
