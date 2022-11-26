27c66159dce7395e4f2cc98e59414771349d7530
reikopi <rk02.stmi.1018@gmail.com>
2022-11-26 04:34:42 +0000

    [Add]java_study_letのスコープ

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,9 +1,10 @@
-var str = "webcamp" //グローバルスコープ
-
-function foo() {
-  console.log(str)
-  var y = "hello" //関数スコープ
-}
+function foo(){
+    let x = "webcamp"
+    {
+        let y = "hello webcamp"
+    }
+    console.log(x)
+    console.log(y)
+ }
 
 foo()
-console.log(y)
