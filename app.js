2a264053c55e84ac149e41ef4ac1f0a09bb6f563
reikopi <rk02.stmi.1018@gmail.com>
2022-11-29 12:39:09 +0000

    [Add]java_study _ジャンケン

--- app.js
+++ app.js
@@ -59,3 +59,9 @@ function addString(strA){
 let promptStr = prompt('何か好きな文字を入力してください。');
 
 alert(promptStr);
+
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');
+
